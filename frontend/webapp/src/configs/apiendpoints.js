d9ed7c3c2883dd5e4564a59163e437d4d8170d2a
Harsh Malviya <malviya.harsh12@gmail.com>
2022-09-27 17:34:43 +0530

    changed context path

--- frontend/webapp/src/configs/apiendpoints.js
+++ frontend/webapp/src/configs/apiendpoints.js
@@ -1,9 +1,8 @@
 const apiendpoints = {
-  login: "/dmu-datastore/data-upload-service/v1/login",
-  upload: "/dmu-datastore/data-upload-service/v1/file/upload",
-  termsAndConditions: "/dmu-datastore/data-upload-service/v1/terms/search",
-  acceptTermsAndConditions:
-    "/dmu-datastore/data-upload-service/v1/terms/accept",
+  login: "/datadaan/v1/login",
+  upload: "/datadaan/v1/file/upload",
+  termsAndConditions: "/datadaan/v1/terms/search",
+  acceptTermsAndConditions: "/datadaan/v1/terms/accept",
 };
 
 export default apiendpoints;
