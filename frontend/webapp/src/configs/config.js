dee68c3723abde2ed325db179668715ba4340a3d
Abhigyan Raman <ramanabhigyan@gmail.com>
2023-04-12 19:59:39 +0000

    modified configuration files

--- frontend/webapp/src/configs/config.js
+++ frontend/webapp/src/configs/config.js
@@ -1,7 +1,7 @@
 const config = {
   BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL
   ? process.env.REACT_APP_APIGW_BASE_URL
-    :"https://datadaan-dibd.centralindia.cloudapp.azure.com/api"
+    :"https://datadaan.bhashini.gov.in/api"
 }
 
 export default config;
