e4806de2b2ec9c0eae7323925ecbaf9cd2557bfa
abhisheksuga <82567892+abhisheksuga@users.noreply.github.com>
2023-05-19 12:20:28 +0530

    Update apiendpoints.js

--- frontend/webapp/src/configs/apiendpoints.js
+++ frontend/webapp/src/configs/apiendpoints.js
@@ -1,9 +1,9 @@
 const apiendpoints = {
-  login: "/datadaan/v1/login",
-  upload: "/datadaan/v1/file/upload",
-  termsAndConditions: "/datadaan/v1/terms/search",
-  acceptTermsAndConditions: "/datadaan/v1/terms/accept",
-  search: "/datadaan/v1/upload/search"
+  login: "/v1/login",
+  upload: "/v1/file/upload",
+  termsAndConditions: "/v1/terms/search",
+  acceptTermsAndConditions: "/v1/terms/accept",
+  search: "/v1/upload/search"
 };
 
 export default apiendpoints;
